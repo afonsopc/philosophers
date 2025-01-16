6278bbac31a673ef3d71f3af54ead475dec10364
Afonso Coutinho <116525378+afonsopc@users.noreply.github.com>
2025-01-16 01:03:55 +0000

    Create structs

--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: afpachec <afpachec@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 23:48:49 by afpachec          #+#    #+#             */
-/*   Updated: 2025/01/15 21:21:30 by afpachec         ###   ########.fr       */
+/*   Updated: 2025/01/16 01:02:52 by afpachec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,37 @@
 # include <unistd.h>
 # include <allocs.h>
 # include <utils.h>
+# include <sys/time.h>
+
+typedef struct timeval	t_time;
+typedef enum e_state
+{
+	THINKING,
+	EATING,
+	SLEEPING,
+	DEAD
+}	t_state;
+
+typedef struct s_philo
+{
+	int			id;
+	t_state		state;
+	t_time		last_meal;
+	t_time		started_eating;
+	t_fork		*left_fork;
+	t_fork		*right_fork;
+	int			meals;
+	void		(*take_fork)(struct s_philo *philo, t_fork *fork);
+	void		(*eat)(struct s_philo *philo);
+	void		(*sleep)(struct s_philo *philo);
+	void		(*think)(struct s_philo *philo);
+	void		(*die)(struct s_philo *philo);
+}	t_philo;
+
+typedef struct s_fork
+{
+	int		id;
+	bool	taken;
+}	t_fork;
 
 #endif
